682748d003d88f673bf0e4d86a00d9937d780045
thona ngon <thona ngon@example.com>
2024-06-11 07:40:07 +0700

    feature the user rigister

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function createUser(Request $request)
+    {
+        try {
+            $validateUser = Validator::make($request->all(), [
+                'name' => 'required|string|max:30',
+                'email' => 'required|string|email|max:50|unique:users',
+                'password' => 'required|string|min:8'
+            ]);
+
+            if ($validateUser->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => $validateUser->errors()
+                ], 400);
+            }
+
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password)
+            ]);
+
+            return response()->json([
+                'status' => true,
+                'data' => $user
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
+    }
+
+    public function listRigisterUsers()
+    {
+        $users = User::all();
+        return response()->json([
+            'users' => $users
+        ], 200);
+    }
+
+    // user login function
+    // public function userLogin(Request $request)
+    // {
+    //     try {
+    //         $validateUser = Validator::make($request->all(), [
+    //             'email' => 'required|string|email|max:255',
+    //             'password' => 'required|string|min:8'
+    //         ]);
+
+    //         if ($validateUser->fails()) {
+    //             return response()->json([
+    //                 'status' => false,
+    //                 'message' => $validateUser->errors()
+    //             ], 400);
+    //         }
+
+    //         if (!Auth::attempt($request->only(['email', 'password']))) {
+    //             return response()->json([
+    //                 'status' => false,
+    //                 'message' => 'Email or password is incorrect'
+    //             ], 401);
+    //         }
+
+    //         $user = User::where('email', $request->email)->first();
+
+    //         return response()->json([
+    //             'status' => true,
+    //             'data' => $user,
+    //             'message' => 'User logged in successfully',
+    //             'token' => $user->createToken('API TOKEN')->plainTextToken
+    //         ], 200);
+    //     } catch (\Throwable $th) {
+    //         return response()->json([
+    //             'status' => false,
+    //             'message' => $th->getMessage()
+    //         ], 500);
+    //     }
+    // }
+
+    // public function listLoginUsers()
+    // {
+    //     $users = User::all();
+    //     return response()->json([
+    //         'users' => $users
+    //     ], 200);
+    // }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('user/register',[UserController::class,'createUser']);
+Route::get('user/register/list',[UserController::class,'listRigisterUsers']);
+// Route::post('user/login',[UserController::class,'userLogin']);
+// Route::get('user/login/list',[UserController::class,'listLoginUsers']);
