0097c9831b655f3fef6c03770ce1f17c8c3940d1
thona ngon <thona ngon@example.com>
2024-06-15 10:27:40 +0700

    resolve conflict rout

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\Api\ProfileController;
 use App\Http\Controllers\Api\commentcontroller;
 use App\Http\Controllers\Api\FriendController;
 use App\Http\Controllers\Api\FriendRequestController;
-use App\Http\Controllers\Api\FriendShipController;
 use App\Http\Controllers\Api\postcontroller;
 use App\Http\Controllers\Api\reactioncontroller;
 use App\Http\Controllers\Api\UserController;
@@ -35,17 +34,28 @@ Route::post('user/login',[UserController::class,'userLogin']);
 Route::get('user/login/list',[UserController::class,'listLoginUsers']);
 
 Route::middleware("auth:sanctum")->group(function(){
+    //login
+    Route::post('user/login',[UserController::class,'userLogin']);
+
+    //post registration
     route::post('post/create', [postcontroller::class, 'store']);
     route::get('post/list', [postcontroller::class, 'index']);
     Route::post('comment/create', [commentcontroller::class, 'store']);
     Route::post('reaction/craete', [reactioncontroller::class, 'store']);
+    //profile
+    Route::put('profile/update/{id}',[ProfileController::class,'update']);
+    //friend
+    Route::get('/friends', [FriendRequestController::class, 'index']);
+    Route::post('/friends', [FriendRequestController::class, 'store']);
+    Route::put('/friends/{id}', [FriendRequestController::class, 'update']);
+    Route::delete('/friends/{id}', [FriendRequestController::class, 'destroy']);
+    Route::get('/friends/{id}', [FriendRequestController::class, 'getFriend']); 
 });
 
 //profile routes
 Route::get('profile/list',[ProfileController::class,'index']);
 Route::post('profile/create',[ProfileController::class,'store']);
 Route::get('profile/show/{id}',[ProfileController::class,'show']);
-Route::put('profile/update/{id}',[ProfileController::class,'update']);
 Route::delete('profile/delete/{id}',[ProfileController::class,'destroy']);
 
 // Friendships
